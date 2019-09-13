857248ea0ee4e53f19e6eb528b5d0ed1250f9645
Anuitex-55 <Anuitex-55@DESKTOP-LOTT593>
2019-09-13 18:38:36 +0300

    api

--- src/redux/login/loginSagas.ts
+++ src/redux/login/loginSagas.ts
@@ -1,78 +1,115 @@
 import { put, takeEvery, call } from "redux-saga/effects";
 import { any } from "prop-types";
 import { delay } from "q";
-
+import  jwt_decode from 'jwt-decode';
  
 export function* doLogin(): IterableIterator<any> {
     yield takeEvery('DO_LOGIN', function*(action: any) {
         
         try {
             let {email, pass, name, isLoading} =  action.data;
+
            
-            let dataLogin = yield call (() => {
-                return fetch('http://localhost:3000/users')
-                        .then(res => res.json())
-                    
-            } )
-            
-            let targetUserLog = dataLogin.filter((i:any) => i.email === email);
-            
-             if (targetUserLog.length === 0) {
-                yield put ({
-                    type: 'ERROR_OCCURED',
-                    payload: {
-                      error: 'User with this email does not exist'
-                      
-                    }
-                  })
-            }  
-            else if (pass === targetUserLog[0].pass) {
+            let req = yield call(() => {
+              return fetch('http://localhost:3000/v1/authenticate', {
+              method: 'POST', 
+              headers: {
+                  'Content-Type': 'application/json',
+                },
+              body: JSON.stringify({email, pass}),
+            })
+            .then((response : any) => response.json())     
+            })
+ 
+            if(req.success){
+              console.log(req.message, req.data)
+              var token = req.data;
               
-                yield put({
-                    type: 'DO_LOADER',
-                    payload: {
-                      isLoader: true
-                    }
-                  });
+              var decoded:any = jwt_decode(token);
+              console.log(decoded.email);
+           
+              // let isLoadi = JSON.stringify(true)
+              // localStorage.setItem('load', isLoadi)
 
-                  let isLoadi = JSON.stringify(true)
-                  localStorage.setItem('load', isLoadi)
-                  localStorage.setItem('id', targetUserLog[0].id)
-                  localStorage.setItem('basket', '[]')
+              //   yield put({
+              //     type: 'LOGIN_SUCCESS',
+              //     payload: {
+              //       // users: dataLogin
+                      
+              //     }
+              // })
+            }
+           
+
+
+
+          //   export function request(url:string,  method: string, body?: object) {
+          //     return (fetch(url, {
+          //         method: method,
+          //         headers: { 'Content-Type': 'application/json' },
+          //         body: JSON.stringify(body)
+          //     }).then((response : any) => {return  response.text().then(function(text :any) {
+          //         return text ? JSON.parse(text) : {}
+          //             }) 
+          //         })
+          //     );
+          // } 
+            
+            // let targetUserLog = dataLogin.filter((i:any) => i.email === email);
+            
+            //  if (req.status === 200) {
+            //      console.log(req.data);
+                 
+              
+            //   // yield put({
+            //   //       type: 'DO_LOADER',
+            //   //       payload: {
+            //   //         isLoader: true
+            //   //       }
+            //   //     });
+
+                 
+            //   //     localStorage.setItem('id', targetUserLog[0].id)
+            //   //     localStorage.setItem('basket', '[]')
                                  
-                yield call(delay, 2000);
+            //   //   yield call(delay, 2000);
 
-                yield put({
-                    type: 'LOGIN_SUCCESS',
-                    payload: {
-                      users: dataLogin
-                        
-                    }
-                })
+                
 
-                yield put({
-                  type: 'GET_DATA_HOME',
-                  payload: {
-                   isAdmin: targetUserLog[0]
-                  }
-                });
+            //     // yield put({
+            //     //   type: 'GET_DATA_HOME',
+            //     //   payload: {
+            //     //    isAdmin: targetUserLog[0]
+            //     //   }
+            //     // });
 
-                yield put({
-                  type: 'GET_USER_DATA',
-                  payload: {
+            //     // yield put({
+            //     //   type: 'GET_USER_DATA',
+            //     //   payload: {
                    
-                  }
-                });
+            //     //   }
+            //     // });
+            // }  
+            else {
+              console.log(req);
+              yield put ({
+                type: 'ERROR_OCCURED',
+                payload: {
+                  error: req.message
+                  
+                }
+              })
+               
             } 
 
-            else {
-                yield put ({
-                    type: 'ERROR_OCCURED',
-                    payload: {
-                      error: 'password is not correct, try again'
-                    }
-                  })
-            }   
+            // else {
+            //     yield put ({
+            //         type: 'ERROR_OCCURED',
+            //         payload: {
+            //           error: 'password is not correct, try again'
+            //         }
+            //       })
+            // }   
         } 
         
         catch (error) {
--- src/redux/registration/registrationSagas.ts
+++ src/redux/registration/registrationSagas.ts
@@ -5,47 +5,52 @@ export function* doInit({}): IterableIterator<any> {
     yield takeEvery(`@@register/DO_REGISTER`, function*(action: any) {
      let {name, email, pass, isAdmin, imgChange} =  action.data;
      try {
-      let dataRegister = yield call (() => { 
-        return fetch('http://localhost:3000/users')
-              .then(res => res.json())
-       })
-        let targetUser = dataRegister.filter((i:any) => i.email === email);
+      // let dataRegister = yield call (() => { 
+      //   return fetch('http://localhost:3000/users')
+      //         .then(res => res.json())
+      //  })
+      //   let targetUser = dataRegister.filter((i:any) => i.email === email);
         
-        if (targetUser.length === 0) {
-              const data = yield call(() => {
-              return fetch('http://localhost:3000/users', {
+        // if (targetUser.length === 0) {
+          let req = yield call(() => {
+                return fetch('http://localhost:3000/v1/register', {
                 method: 'POST', 
                 headers: {
                     'Content-Type': 'application/json',
                   },
                 body: JSON.stringify({name, email, pass, isAdmin, imgChange}),
-              })      
+              }).then((response : any) => response.json())       
               })
+              console.log(req);
+
+              if (req.success) {
+                  yield put({
+                  type: `@@register/SUCCESSFULL`,
+                  payload: {
+                    email: email,
+                    pass: pass,
+                    isRegister: true,
+                    
+                  }
+                });
               
-              yield put({
-                type: 'DO_LOADER',
-                payload: {
-                  isLoader: true
-                }
-              });
-              yield call(delay, 3000);
+              }
               
-              yield put({
-                type: `@@register/SUCCESSFULL`,
-                payload: {
-                  email: email,
-                  pass: pass,
-                  isRegister: true,
-                  
-                }
-              });
+              // yield put({
+              //   type: 'DO_LOADER',
+              //   payload: {
+              //     isLoader: true
+              //   }
+              // });
+              // yield call(delay, 3000);
               
-        }
+             
+        // }
         else {
           yield put ({
             type: 'ERROR_OCCURED',
             payload: {
-              error: 'User with this data already exist, enter another data'
+              error: req.message
               
             }
           })
@@ -53,7 +58,9 @@ export function* doInit({}): IterableIterator<any> {
         } 
      
      }
-     catch(e){
+     catch(error){
+      yield console.log(error);
+       
       yield put({
         type: `@@ERROR_OCCURED`,
         payload: {
